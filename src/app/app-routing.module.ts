0cdd62c7fa4a1e854c940fd1d065a582aa150bf9
webdeveloper <99655hari@gmail.com>
2023-10-03 05:55:53 +0530

    3rd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { GadgethomeComponent } from './Components/gadgethome/gadgethome.component';
 
 const routes: Routes = [
-  {path:'/',redirectTo: '/home',pathMatch:'full'},
+  {path:' ',redirectTo: '/home',pathMatch:'full'},
   {path:'home',component:GadgethomeComponent}
 ];
 
