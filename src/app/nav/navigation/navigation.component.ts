83358d42bfe36b9c9566d5201a23e3939979f7f9
webdeveloper <99655hari@gmail.com>
2023-10-10 20:57:29 +0530

    validation2

--- src/app/nav/navigation/navigation.component.ts
+++ src/app/nav/navigation/navigation.component.ts
@@ -49,7 +49,7 @@ export class DialogElementsExampleDialog {
   LoginForm !:FormGroup;
   Hide:boolean = true;
   errormsg = '';
-  inputtype='text';
+  inputtype = 'email';
   suffix='';
   constructor(private formbuilder:FormBuilder){
     this.LoginForm = this.formbuilder.group({
@@ -58,32 +58,35 @@ export class DialogElementsExampleDialog {
     });
     
   }
-  
-  ngOnInit(){
-  }
-
-    change(){
-      if(this.LoginForm.controls['emailorphone'].value != ' '){
-        if (this.LoginForm.controls['emailorphone'].value.match(/^(?!(\d)\1+$)(?:\(?\+\d{1,3}\)?[- ]?|0)?\d{10}$/)) {
-          this.inputtype='tel';
-          this.suffix='phone'
-          this.LoginForm.controls['emailorphone'].addValidators([Validators.pattern(/^(?!(\d)\1+$)(?:\(?\+\d{1,3}\)?[- ]?|0)?\d{10}$/)]);
-          // this.errormsg = 'Enater a valid Mobile Number';
-          console.log(this.LoginForm.controls['emailorphone'].value)
-          // alert(this.LoginForm.controls['emailorphone'].value);
-          // this.LoginForm.controls['OTP'].addValidators(Validators.required)
-          } 
-          else {
-          this.inputtype='email';
-          this.suffix='mail';
-          this.LoginForm.controls['emailorphone'].addValidators([Validators.email]);
-          this.errormsg = 'Enater a valid email';
-          this.LoginForm.controls['OTP'].addValidators(Validators.required);
-          // alert(this.LoginForm.controls['emailorphone'].value);
-          }
+  change(){
+    if(this.LoginForm.controls['emailorphone'].value != ' '){
+      
+      if(this.LoginForm.controls['emailorphone'].value.match(/^(?!(\d)\1+$)(?:\(?\+\d{1,3}\)?[- ]?|0)?\d{10}$/)){
+        // console.log(this.LoginForm.controls['emailorphone'].value);
+          this.inputtype='text';
+          this.suffix='phone';
+          console.log("pattern added");
+          this.LoginForm.controls['emailorphone'].setValidators([Validators.pattern(/^(?!(\d)\1+$)(?:\(?\+\d{1,3}\)?[- ]?|0)?\d{10}$/),Validators.minLength(10)]);
+          this.LoginForm.controls['emailorphone'].removeValidators([Validators.email]);
+          if(this.LoginForm.valid) this.Hide =!this.Hide;
+          if(this.LoginForm.controls['emailorphone'].hasError('Pattern')) this.errormsg = 'Enter a Valid MobileNumber';
+          console.log("email removed");
+      }
+      else {
+        this.inputtype = 'email';
+        this.suffix='mail';
+        console.log("emailset")
+        this.LoginForm.controls['emailorphone'].setValidators([Validators.email,Validators.minLength(10)]);
+        if(this.LoginForm.controls['emailorphone'].hasError('email')){this.errormsg = 'Enater a valid email';}
+        this.LoginForm.controls['emailorphone'].removeValidators([Validators.pattern(/^(?!(\d)\1+$)(?:\(?\+\d{1,3}\)?[- ]?|0)?\d{10}$/)]);
+        if(this.LoginForm.valid) this.Hide =!this.Hide;
+        console.log("patternremoved");
+        // else this.LoginForm.controls['emailorphone'].removeValidators(Validators.email);
       }
     }
-    
-  
+    else{
+      this.LoginForm.invalid;
+    }
+  }
 
 }
